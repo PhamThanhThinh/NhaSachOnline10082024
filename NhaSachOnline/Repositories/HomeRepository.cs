f554394ea20334cf93ac69a5e36dac66d113b9d4
thinh <thinhqn999@gmail.com>
2024-08-10 23:08:03 +0700

    23h07

---
+++ NhaSachOnline/Repositories/HomeRepository.cs
@@ -0,0 +1,24 @@
+﻿using NhaSachOnline.Data;
+using NhaSachOnline.Models;
+
+namespace NhaSachOnline.Repositories
+{
+  public class HomeRepository
+  {
+    private readonly ApplicationDbContext _dbContext;
+
+    public HomeRepository(ApplicationDbContext dbContext)
+    {
+      _dbContext = dbContext;
+    }
+
+    //public async Task<IEnumerable<Book>> LayThongTinSachTuDatabase(string keySearch = "", int theLoaiId = 0)
+    //{
+    //  // chuyển đổi chuỗi sang dạng chữ thường
+    //  keySearch = keySearch.ToLower();
+
+    //  IEnumerable<Book> books = await ().ToListAsync();
+    //  return books;
+    //}
+  }
+}
